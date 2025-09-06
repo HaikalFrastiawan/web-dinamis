46e336c178901f90af13d7a5138912d1e89d5866
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-09-06 16:19:08 +0700

    if-else

--- express-ejs/index.js
+++ express-ejs/index.js
@@ -6,12 +6,17 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views')) //bisa mengambil file dari luar folder
 
 app.get('/', (req,res) =>{
-        res.render('home')
+    res.render('home')
+})
+
+app.get('/t/:tag', (req,res) =>{
+    const { tag } = req.params
+    res.render('tag', {tag})
 })
 
 app.get('/rand', (req,res) =>{
-     const num = Math.floor(Math.random() * 10) + 1
-        res.render('random', {random: num})
+    const num = Math.floor(Math.random() * 10) + 1
+    res.render('random', {random: num})
 })
 
 app.listen(8080, () =>{
