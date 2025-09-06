9b3231a5ac0b33e0951984626fcf30505f904167
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-09-06 16:00:46 +0700

    passing data ke template

--- express-ejs/index.js
+++ express-ejs/index.js
@@ -1,11 +1,17 @@
+const path = require ('path')
 const express = require ('express')
 const app = express()
 
 app.set('view engine', 'ejs')
-
+app.set('views', path.join(__dirname, '/views')) //bisa mengambil file dari luar folder
 
 app.get('/', (req,res) =>{
-        res.render('home.ejs')
+        res.render('home')
+})
+
+app.get('/rand', (req,res) =>{
+     const num = Math.floor(Math.random() * 10) + 1
+        res.render('random', {random: num})
 })
 
 app.listen(8080, () =>{
