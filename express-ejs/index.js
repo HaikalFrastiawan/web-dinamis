408ab8b9be3b91f60c8c8ab7523343de6da8f3f1
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-09-06 17:10:53 +0700

    finish

--- express-ejs/index.js
+++ express-ejs/index.js
@@ -2,16 +2,34 @@ const path = require ('path')
 const express = require ('express')
 const app = express()
 
+const tagsData = require ('./data.json')
+
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views')) //bisa mengambil file dari luar folder
+
+app.use(express.static(path.join(__dirname, '/public')))
+
 
 app.get('/', (req,res) =>{
     res.render('home')
 })
 
 app.get('/t/:tag', (req,res) =>{
+
     const { tag } = req.params
-    res.render('tag', {tag})
+
+    const data = tagsData[tag]
+    if( data ) {
+        res.render('tag', {data})
+    } else{
+        res.render('notfound', {tag})
+    }
+})
+
+app.get('/cats', (req,res) =>{
+    const cats = ['oyen', 'embul', 'black']
+    res.render('cats', {cats})
 })
 
 app.get('/rand', (req,res) =>{
