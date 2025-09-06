e4a138d109c2b2eb7eab4ac4daf6d0599b549395
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-09-06 15:46:13 +0700

    setup

---
+++ express-ejs/index.js
@@ -0,0 +1,15 @@
+const express = require ('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req,res) =>{
+        res.render('home.ejs')
+})
+
+app.listen(8080, () =>{
+    console.log(`Listening on host http://localhost:8080`)
+})
+
+
